a3b0a617f33480ebf2532e784cdfc1ac37cba741
chris_maker_ <christian92iannella@gmail.com>
2023-10-11 11:44:02 +0200

    Upload

--- Firmware/v1.0/src/main.cpp
+++ Firmware/v1.0/src/main.cpp
@@ -2,9 +2,9 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-#define RING_PIN 10
+#define LED_PIN 10
 #define NUM_LED 4
-Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_LED, RING_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_LED, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 #define E 2
 #define D 3
@@ -26,53 +26,61 @@ int i2c_address = 0;
 
 unsigned long previous = 0;
 unsigned long previous_dp_blink = 0;
+long firstPixelHue = 0;
+unsigned long t = 0;
 
 int b_l_value = 0;
 bool state = false;
 bool dp_blynk_state = false;
 unsigned long dp_delay = 20;
 bool dp = false;
+bool rainbow_state = false;
+int rainbow_speed = 0;
 int digit = 10;
 
-// First byte  Second byte     Symbol
-void print_zero();      // 0           (0 or 1)        0
-void print_one();       // 1           (0 or 1)        1
-void print_two();       // 2           (0 or 1)        2
-void print_three();     // 3           (0 or 1)        3
-void print_four();      // 4           (0 or 1)        4
-void print_five();      // 5           (0 or 1)        5
-void print_six();       // 6           (0 or 1)        6
-void print_seven();     // 7           (0 or 1)        7
-void print_eight();     // 8           (0 or 1)        8
-void print_nine();      // 9           (0 or 1)        9
+//                      First byte  Second byte     Symbol
+void print_space();     // 32             x          space
+void print_zero();      // 48          (0 or 1)        0
+void print_one();       // 49          (0 or 1)        1
+void print_two();       // 50          (0 or 1)        2
+void print_three();     // 51          (0 or 1)        3
+void print_four();      // 52          (0 or 1)        4
+void print_five();      // 53          (0 or 1)        5
+void print_six();       // 54          (0 or 1)        6
+void print_seven();     // 55          (0 or 1)        7
+void print_eight();     // 56          (0 or 1)        8
+void print_nine();      // 57          (0 or 1)        9
 void erase();           // 10             x
-void show_dp();         // 11             x            .
-void print_dash();      // 12          (0 or 1)        -
-void print_degrees();   // 13          (0 or 1)        ˚
-void print_A();         // 14          (0 or 1)        A
-void print_b();         // 15          (0 or 1)        b
-void print_c();         // 16          (0 or 1)        c
-void print_C();         // 17          (0 or 1)        C
-void print_d();         // 18          (0 or 1)        d
-void print_E();         // 19          (0 or 1)        E
-void print_F();         // 20          (0 or 1)        F
-void print_H();         // 21          (0 or 1)        H
-void print_I();         // 22          (0 or 1)        I
-void print_L();         // 23          (0 or 1)        L
-void print_P();         // 24          (0 or 1)        P
-void print_U();         // 25          (0 or 1)        U
-void print_u();         // 26          (0 or 1)        u
+void show_dp();         // 46             x            .
+void print_dash();      // 45          (0 or 1)        -
+void print_degrees();   // 20          (0 or 1)        ˚
+void print_A();         // 65          (0 or 1)        A
+void print_b();         // 98          (0 or 1)        b
+void print_c();         // 99          (0 or 1)        c
+void print_C();         // 67          (0 or 1)        C
+void print_d();         // 100         (0 or 1)        d
+void print_E();         // 69          (0 or 1)        E
+void print_F();         // 70          (0 or 1)        F
+void print_H();         // 72          (0 or 1)        H
+void print_I();         // 73          (0 or 1)        I
+void print_L();         // 76          (0 or 1)        L
+void print_O();         // 79          (0 or 1)        O
+void print_P();         // 80          (0 or 1)        P
+void print_S();         // 83          (0 or 1)        S
+void print_U();         // 85          (0 or 1)        U
+void print_u();         // 117         (0 or 1)        u
 void print_segment_a(); // 27          (0 or 1)
 void print_segment_b(); // 28          (0 or 1)
 void print_segment_c(); // 29          (0 or 1)
 void print_segment_d(); // 30          (0 or 1)
 void print_segment_e(); // 31          (0 or 1)
-void print_segment_f(); // 32          (0 or 1)
+void print_segment_f(); // 40          (0 or 1)
 void print_segment_g(); // 33          (0 or 1)
-void dp_blink();        // 34          (1 to 7)
-void dp_blink_stop();   // 35          (0 or 1)
-void back_light();      // 36          (0 to 255)       Backlight color HSV
-void brightness();      // 37          (0 to 255)       Backlight brightness
+                        // 34          (1 to 7)         dp blink
+                        // 35          (0 or 1)         dp stop
+                        // 37          (0 to 255)       Backlight brightness
+                        // 38          (1 to 255)       Rainbow
+                        // 39             x             erase all
 
 void receiveEvent(int data)
 {
@@ -136,6 +144,7 @@ void receiveEvent(int data)
     }
     else if (first_byte == 36)
     {
+      rainbow_state = false;
       for (int i = 0; i <= NUM_LED; i++)
       {
         pixel.setPixelColor(i, pixel.gamma32(pixel.ColorHSV(second_byte * 254)));
@@ -146,6 +155,23 @@ void receiveEvent(int data)
     {
       pixel.setBrightness(second_byte);
       pixel.show();
+    }
+    else if (first_byte == 38)
+    {
+      rainbow_state = true;
+      rainbow_speed = second_byte;
+    }
+    else if (first_byte == 39)
+    {
+      rainbow_state = false;
+      dp_blynk_state = false;
+      dp = false;
+      for (int i = 0; i <= NUM_LED; i++)
+      {
+        pixel.setPixelColor(i,0,0,0);
+      }
+      pixel.show();
+      digit = 10;
     }
     else
     {
@@ -210,6 +236,8 @@ void loop()
   unsigned long current_dp_blink = millis();
   unsigned long current = millis();
 
+  raimbow();
+
   if (dp_blynk_state == true)
   {
     if (current_dp_blink - previous_dp_blink >= dp_delay)
@@ -225,43 +253,47 @@ void loop()
     state = !state;
     digitalWrite(COM, state);
 
-    if (digit == 0)
+    if (digit == 48)
     {
       print_zero();
     }
-    else if (digit == 1)
+    else if (digit == 32)
+    {
+      print_space();
+    }
+    else if (digit == 49)
     {
       print_one();
     }
-    else if (digit == 2)
+    else if (digit == 50)
     {
       print_two();
     }
-    else if (digit == 3)
+    else if (digit == 51)
     {
       print_three();
     }
-    else if (digit == 4)
+    else if (digit == 52)
     {
       print_four();
     }
-    else if (digit == 5)
+    else if (digit == 53)
     {
       print_five();
     }
-    else if (digit == 6)
+    else if (digit == 54)
     {
       print_six();
     }
-    else if (digit == 7)
+    else if (digit == 55)
     {
       print_seven();
     }
-    else if (digit == 8)
+    else if (digit == 56)
     {
       print_eight();
     }
-    else if (digit == 9)
+    else if (digit == 57)
     {
       print_nine();
     }
@@ -269,67 +301,75 @@ void loop()
     {
       erase();
     }
-    else if (digit == 11)
+    else if (digit == 46)
     {
       show_dp();
     }
-    else if (digit == 12)
+    else if (digit == 45)
     {
       print_dash();
     }
-    else if (digit == 13)
+    else if (digit == 20)
     {
       print_degrees();
     }
-    else if (digit == 14)
+    else if (digit == 65)
     {
       print_A();
     }
-    else if (digit == 15)
+    else if (digit == 98)
     {
       print_b();
     }
-    else if (digit == 16)
+    else if (digit == 99)
     {
       print_c();
     }
-    else if (digit == 17)
+    else if (digit == 67)
     {
       print_C();
     }
-    else if (digit == 18)
+    else if (digit == 100)
     {
       print_d();
     }
-    else if (digit == 19)
+    else if (digit == 69)
     {
       print_E();
     }
-    else if (digit == 20)
+    else if (digit == 70)
     {
       print_F();
     }
-    else if (digit == 21)
+    else if (digit == 72)
     {
       print_H();
     }
-    else if (digit == 22)
+    else if (digit == 73)
     {
       print_I();
     }
-    else if (digit == 23)
+    else if (digit == 76)
     {
       print_L();
     }
-    else if (digit == 24)
+    else if (digit == 79)
+    {
+      print_O();
+    }
+    else if (digit == 80)
     {
       print_P();
     }
-    else if (digit == 25)
+    else if (digit == 83)
+    {
+      print_S();
+    }
+    else if (digit == 85)
     {
       print_U();
     }
-    else if (digit == 26)
+    else if (digit == 117)
     {
       print_u();
     }
@@ -353,7 +393,7 @@ void loop()
     {
       print_segment_e();
     }
-    else if (digit == 32)
+    else if (digit == 40)
     {
       print_segment_f();
     }
@@ -363,7 +403,17 @@ void loop()
     }
   }
 }
-
+void print_space()
+{
+  digitalWrite(A, state);
+  digitalWrite(B, state);
+  digitalWrite(C, state);
+  digitalWrite(D, state);
+  digitalWrite(E, state);
+  digitalWrite(F, state);
+  digitalWrite(G, state);
+  digitalWrite(DP, state);
+}
 void print_zero()
 {
   digitalWrite(A, !state);
@@ -798,6 +848,24 @@ void print_L()
     digitalWrite(DP, state);
   }
 }
+void print_O()
+{
+  digitalWrite(A, !state);
+  digitalWrite(B, !state);
+  digitalWrite(C, !state);
+  digitalWrite(D, !state);
+  digitalWrite(E, !state);
+  digitalWrite(F, !state);
+  digitalWrite(G, state);
+  if (dp == true)
+  {
+    digitalWrite(DP, !state);
+  }
+  else
+  {
+    digitalWrite(DP, state);
+  }
+}
 void print_P()
 {
   digitalWrite(A, !state);
@@ -805,6 +873,24 @@ void print_P()
   digitalWrite(C, state);
   digitalWrite(D, state);
   digitalWrite(E, !state);
+  digitalWrite(F, !state);
+  digitalWrite(G, !state);
+  if (dp == true)
+  {
+    digitalWrite(DP, !state);
+  }
+  else
+  {
+    digitalWrite(DP, state);
+  }
+}
+void print_S()
+{
+  digitalWrite(A, !state);
+  digitalWrite(B, state);
+  digitalWrite(C, !state);
+  digitalWrite(D, !state);
+  digitalWrite(E, state);
   digitalWrite(F, !state);
   digitalWrite(G, !state);
   if (dp == true)
@@ -976,5 +1062,28 @@ void print_segment_g()
   else
   {
     digitalWrite(DP, state);
+  }
+}
+
+void raimbow()
+{
+  if (rainbow_state == true)
+  {
+    if (millis() > t + 1+(1*rainbow_speed))
+    {
+      t = millis();
+      for (int i = 0; i < pixel.numPixels(); i++)
+      {
+        int pixelHue = firstPixelHue  + (i * 65536L / pixel.numPixels());
+        // int pixelHue = firstPixelHue + (65536L / ring.numPixels());
+        pixel.setPixelColor(i, pixel.gamma32(pixel.ColorHSV(pixelHue)));
+      }
+      pixel.show();
+      if (firstPixelHue > 5 * 65536)
+      {
+        firstPixelHue = 1000 * i2c_address;
+      }
+      firstPixelHue += 256;
+    }
   }
 }
